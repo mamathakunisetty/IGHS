4d0810d5fb21fdae8bd6b91526c436f02f1dd484
Mamatha Kunisetty <108059215+mamathakunisetty@users.noreply.github.com>
2025-10-03 17:01:20 +0530

    program.cs

---
+++ program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+using Microsoft.VisualStudio.Coverage.Analysis;
+
+namespace CodeCoverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coverageFilePath = string.Empty;
+            List<XElement> elements = new List<XElement>();
+            string coveragePath = string.Empty;
+            string lookupPath = string.Empty;
+
+#if DEBUG
+
+            coveragePath = (from fileInfo in (new DirectoryInfo(@"..\..\")).GetFiles("data.coverage", SearchOption.AllDirectories)
+                            orderby fileInfo.CreationTime descending
+                            select fileInfo.FullName).First();
+            lookupPath = string.Format(@"{0}\out\", Directory.GetParent(Directory.GetParent(Directory.GetParent(coveragePath).ToString()).ToString()).ToString());
+#else
+           DirectoryInfo dirInfo = new DirectoryInfo(@"..\");
+           coverageFilePath = (from fileInfo in dirInfo.GetFiles("data.coverage", SearchOption.AllDirectories)
+                                       select fileInfo.FullName).FirstOrDefault();
+           coveragePath = string.Format(@"{0}\In\{1}\data.coverage", Directory.GetParent(Directory.GetParent(Directory.GetParent(coverageFilePath).ToString()).ToString()).ToString(), Environment.MachineName);
+            lookupPath = string.Format(@"{0}\out\", Directory.GetParent(Directory.GetParent(Directory.GetParent(coverageFilePath).ToString()).ToString()).ToString());
+#endif
+
+            if ((!string.IsNullOrWhiteSpace(coveragePath)) && (!string.IsNullOrWhiteSpace(lookupPath)))
+            {
+                IEnumerable<Header> header = Util.GetHeaders(coveragePath, lookupPath);
+                IEnumerable<Category> parent = Util.GetParentCategories(header);
+                IEnumerable<Category> children = Util.GetSubCategories(header);
+                Util.Process(parent, children);
+            }
+            else
+            {
+                Console.WriteLine("Lookup path is not set");
+            }
+        }
+    }
+}
